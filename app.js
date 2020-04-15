f825fddc3e5e7ff12772c4531e2b1371a4a812a0
Felix Negoita <felixnego@gmail.com>
2020-04-15 14:49:23 +0300

    added user model and passport configs

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const LocalStrategy = require('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
+const User = require('./models/user')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/express_auth_demo')
 
+app.use(require('express-session')({
+    secret: 'Nobody expected the Spanish Inquisition',
+    resave: false,
+    saveUninitialized: false
+}))
+
 app.set('view engine', 'ejs')
+app.use(passport.initialize())
+app.use(passport.session())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get('/', (req, res) => {
     res.render('home')
